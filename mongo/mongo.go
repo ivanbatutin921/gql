eeb2cbc3b2a1dbd7fa5da27b164bea6b94e1334e
Ivan <ivanbatutin921@mail.ru>
2024-10-20 08:25:07 +0500

    mongo

---
+++ mongo/mongo.go
@@ -0,0 +1,59 @@
+package mongodb
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"root/configs"
+	"root/graph/model"
+)
+
+type DB struct {
+	client *mongo.Client
+}
+
+func ConnectDB() *DB {
+	client, err := mongo.NewClient(options.Client().ApplyURI(configs.EnvMongoURI()))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Ping the database
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB")
+	return &DB{client: client}
+}
+
+// colHelper возвращает коллекцию по имени
+func colHelper(db *DB, collectionName string) *mongo.Collection {
+	return db.client.Database("projectMngt").Collection(collectionName)
+}
+
+// InsertMessage вставляет новое сообщение в коллекцию
+func (db *DB) InsertMessage(message *model.Message) error {
+	collection := colHelper(db, "messages") // Используем colHelper для получения коллекции
+
+	_, err := collection.InsertOne(context.Background(), bson.M{
+		"id":   message.ID,
+		"text": message.Text,
+	})
+	log.Println("вроде норм бд")
+	return err
+}
+
